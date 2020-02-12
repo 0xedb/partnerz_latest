f3293291a149dba8a9d342433e70c6b5be987474
theBashShell <edohbruno@gmail.com>
2020-02-12 14:16:21 +0000

    top editing

--- src/components/index/Top.tsx
+++ src/components/index/Top.tsx
@@ -4,7 +4,13 @@ import Stock from "../stock/Stock";
 const Top = () => {
   return (
     <div className={style.top}>
-      <div className={style.text}>...</div>
+      <div className={style.text}>
+        <h2>We're the Financial Architects</h2>
+        <p>
+          At Partnerz, we engineer and bring to life the financial goals and
+          aspirations of hardworking people
+        </p>
+      </div>
     </div>
   );
 };
--- src/components/nav/NavBar.tsx
+++ src/components/nav/NavBar.tsx
@@ -1,20 +1,10 @@
 import Link from "next/link";
-import { useEffect, useState } from "react";
 import style from "./css/navbar.module.css";
 import Stock from "../stock/Stock";
 
 const NavBar = () => {
-  const [navStyle, setNavStyle] = useState(null);
-
-  useEffect(() => {
-    window.addEventListener("scroll", () => {
-      window.pageYOffset > 10
-        ? setNavStyle(style.nav_style)
-        : setNavStyle(null);
-    });
-  });
   return (
-    <nav className={`${style.nav} ${navStyle}`}>
+    <nav className={style.nav}>
       <Link href="/">
         <a>
           <img
@@ -24,7 +14,7 @@ const NavBar = () => {
           />
         </a>
       </Link>
-      <div className={`${style.stock} ${navStyle}`}>
+      <div className={style.stock}>
         <Stock />
       </div>
     </nav>
