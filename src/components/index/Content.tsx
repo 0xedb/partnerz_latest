bfbb342987f76d268fd8797f6b02d7f459e49342
theBashShell <edohbruno@gmail.com>
2020-02-12 14:47:03 +0000

    content

---
+++ src/components/index/Content.tsx
@@ -0,0 +1,13 @@
+import style from "./css/content.module.css";
+const Content = () => {
+  return (
+    <section className={style.content}>
+      <div className={`${style.tag} center_t`}>
+        <p> Financial stability for everyone, by the experts</p>
+      </div>
+      <div>adsffkasdl</div>
+    </section>
+  );
+};
+
+export default Content;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,15 @@
 import Top from "../components/index/Top";
 import HeadMeta from "../components/meta/HeadMeta";
 import NavBar from "../components/nav/NavBar";
+import Content from "../components/index/Content";
 const Index = () => (
   <>
     <HeadMeta>
       <title>Partnerz FP Limited | Your Financial Architects</title>
     </HeadMeta>
-    <NavBar /> 
+    <NavBar />
     <Top />
+    <Content />
     <div style={{ height: "700px" }}>more</div>
   </>
 );
