8e55ca4d630c303ce5bd268f642b381fda795763
theBashShell <edohbruno@gmail.com>
2020-02-12 17:17:30 +0000

    hover fixed

--- src/components/index/Services.tsx
+++ src/components/index/Services.tsx
@@ -1,13 +1,26 @@
+import { useState } from "react";
 import style from "./css/services.module.css";
 import CONSTANTS from "../../util/constants.json";
 
-const Service = ({ head, pic, desc }: PartnerService) => (
-  <div className={style.service}>
-    <img src={pic} alt={head} />
-    <h2>{head}</h2>
-    <p>{desc}</p>
-  </div>
-);
+const Service = ({ head, pic, desc }: PartnerService) => {
+  const [hover, setHover] = useState(false);
+  const handleHover = () => {
+    setHover(prev => !prev);
+  };
+
+  return (
+    <div
+      className={style.service}
+      onMouseEnter={handleHover}
+      onMouseLeave={handleHover}
+    >
+      <div className={`${style.filler} ${hover ? style.fill : ""}`} />
+      <img src={pic} alt={head} />
+      <h3>{head}</h3>
+      <p>{desc}</p>
+    </div>
+  );
+};
 
 const Services = () => {
   return (
