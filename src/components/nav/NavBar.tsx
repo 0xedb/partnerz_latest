3d19495caae58c10982365ed74feb7d0174bfe3b
theBashShell <edohbruno@gmail.com>
2020-02-15 14:49:59 +0000

    menu added

--- src/components/nav/NavBar.tsx
+++ src/components/nav/NavBar.tsx
@@ -1,8 +1,14 @@
 import Link from "next/link";
 import style from "./css/navbar.module.css";
 import CONSTANTS from "../../util/constants.json";
+import { useState } from "react";
 
 const NavBar = () => {
+  const [overlay, setOverlay] = useState(false);
+  const handleOverlay = () => {
+    setOverlay(prev => !prev);
+  };
+
   return (
     <nav className={`sec ${style.nav}`}>
       <Link href="/">
@@ -15,7 +21,11 @@ const NavBar = () => {
         </a>
       </Link>
       <div>
-        <img className={`center_t ${style.menu}`} src={CONSTANTS.images.menu} />
+        <img
+          className={`center_t ${style.menu}`}
+          onClick={handleOverlay}
+          src={overlay ? CONSTANTS.images.close : CONSTANTS.images.menu}
+        />
       </div>
     </nav>
   );
