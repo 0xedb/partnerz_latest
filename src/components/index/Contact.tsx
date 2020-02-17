e6b96eaa99ce22b60255b22b303fb4800dc354e4
theBashShell <edohbruno@gmail.com>
2020-02-17 16:14:39 +0000

    contact progress

--- src/components/index/Contact.tsx
+++ src/components/index/Contact.tsx
@@ -1,11 +1,13 @@
-import style from './css/contact.module.css'
+import style from "./css/contact.module.css";
 
 const Contact = () => {
   return (
     <div className={style.contact}>
-      contac us
+      <p>A journey of a thousand miles begins with a single step</p>
+      <p>Begin your financial journey here</p>
+      
     </div>
   );
-}
+};
 
 export default Contact;
