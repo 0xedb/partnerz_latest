5c0a986eeb5b3b87c85f0787a001bfd0560b19eb
theBashShell <edohbruno@gmail.com>
2020-02-17 14:45:42 +0000

    form page changes

--- src/pages/form.tsx
+++ src/pages/form.tsx
@@ -1,5 +1,22 @@
+import Top from "../components/index/Top";
+import HeadMeta from "../components/meta/HeadMeta";
+import NavBar from "../components/nav/NavBar";
+import Content from "../components/index/Content";
+import Footer from "../components/footer/Footer";
+import Page from "../components/page/Page";
+
 const form = () => {
-  return <div>form here</div>;
+  return (
+    <div>
+      <Page>
+        <HeadMeta>
+          <title>Partnerz FP Limited | Your Financial Architects</title>
+        </HeadMeta>
+        <NavBar />
+        form here 
+      </Page>
+    </div>
+  );
 };
 
 export default form;
