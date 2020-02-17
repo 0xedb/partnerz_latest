4b6cb37aa2d31f1f1615ec97c0531fbbd50401d3
theBashShell <edohbruno@gmail.com>
2020-02-17 13:22:12 +0000

    relevant footer info

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -2,8 +2,10 @@ import style from "./css/footer.module.css";
 
 const Footer = () => {
   return (
-    <footer className={style.footer}> 
+    <footer className={style.footer}>
       <address>
+        <a href="tel:+233504461779">0504461779</a>
+        <a href="mailto:hello@mypartnerz-fp.com">hello@mypartnerz-fp.com</a>
         <div>Plot No. 21 Airport City, Accra</div>
         <div>&copy;{new Date().getFullYear()} Partnerz FP Limited</div>
       </address>
