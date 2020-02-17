326a10c2243b78277517d3872e0507d78e43c274
theBashShell <edohbruno@gmail.com>
2020-02-17 16:39:17 +0000

    contact form + office fabric

--- src/components/index/Contact.tsx
+++ src/components/index/Contact.tsx
@@ -1,11 +1,17 @@
+import Link from "next/link";
 import style from "./css/contact.module.css";
+import { PrimaryButton } from "office-ui-fabric-react";
 
 const Contact = () => {
   return (
     <div className={style.contact}>
       <p>A journey of a thousand miles begins with a single step</p>
       <p>Begin your financial journey here</p>
-      
+      <Link href="/form">
+        <a>
+          <PrimaryButton text="Start" className="center_t" />
+        </a>
+      </Link>
     </div>
   );
 };
