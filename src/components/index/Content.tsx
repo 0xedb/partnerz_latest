e86a7fcb6cc93ca797d307fe9779e6576d9d1776
theBashShell <edohbruno@gmail.com>
2020-02-18 14:36:17 +0000

    quote separated

--- src/components/index/Content.tsx
+++ src/components/index/Content.tsx
@@ -7,19 +7,24 @@ import Contact from "./Contact";
 const Content = () => {
   return (
     <section className={`centered ${style.content}`}>
-      <h3>Financial stability for everyone, by the experts</h3>
-      <div className={`${style.quote} centered`}>
-        <img src={CONSTANTS.images.quote} alt="quote" />
+      <h3 className={style.head}>
+        Financial stability for everyone, by the experts
+      </h3>
+      <div className={style.say}>
+        <div className={`${style.quote} centered`}>
+          <img src={CONSTANTS.images.quote} alt="quote" />
+        </div>
+        <p className={style.statement}>
+          Partnerz FP is an advisory firm with a daring quest to use financial
+          structures to affect lives positively. We are responsible for crafting
+          world-class financial solutions that touches individuals, corporates,
+          and governments. Partnerz FP is an advisory firm with a daring quest
+          to use financial structures to affect lives positively. We are
+          responsible for crafting world-class financial solutions that touches
+          individuals, corporates, and governments.
+        </p>
       </div>
-      <p className={style.statement}>
-        Partnerz FP is an advisory firm with a daring quest to use financial
-        structures to affect lives positively. We are responsible for crafting
-        world-class financial solutions that touches individuals, corporates,
-        and governments. Partnerz FP is an advisory firm with a daring quest to
-        use financial structures to affect lives positively. We are responsible
-        for crafting world-class financial solutions that touches individuals,
-        corporates, and governments.
-      </p>
+
       <Services />
       <Feedback />
       <Contact />
